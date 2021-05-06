69fef8055fd95e0fb1e6841b7e1a15bcb4f83abe
CamTu <camtu@jv-it.com.vn>
2021-05-06 19:25:05 +0700

    test

--- test/pageobjects/cloud.page.js
+++ test/pageobjects/cloud.page.js
@@ -1,12 +1,18 @@
 const Page = require("./page");
 const fetch = require("node-fetch");
+const path = require("path");
 const assert = require("assert");
-const { ImageAnnotatorClient } = require("@google-cloud/vision");
-const download = require("image-downloader");
+const tesseract = require("node-tesseract-ocr");
 
-const client = new ImageAnnotatorClient({
-  keyFilename: "./ggconfig.json",
-});
+
+const download = require("image-downloader");
+// const vision = require("@google-cloud/vision");
+
+// const client = new vision.ImageAnnotatorClient({
+//   keyFilename: "../Demo_TTTN/test/mock/tttn-img.json",
+// });
+
+
 let x = null;
 class CloudPage extends Page {
   get btnResetPassword() {
@@ -22,14 +28,20 @@ class CloudPage extends Page {
     return $(".form-control-feedback img");
   }
   get inputImg() {
-    return $("input[id=input]");
+    return $("input#input");
   }
   get robot() {
     return $(".recaptcha-checkbox-border");
   }
+  get testTry() {
+    return $("h2#try-it-for-yourself");
+  }
+  // get textArea() {
+  //   return $("textarea#extracted_text");
+  // }
   async reset(emailUser) {
     // await (await this.btnResetPassword).click();
-    await (await this.inputEmail).setValue();
+    await (await this.inputEmail).setValue(emailUser);
     await (await this.roleResetPass).selectByAttribute("value", "svcq");
   }
 
@@ -38,23 +50,34 @@ class CloudPage extends Page {
     return x;
   }
 
-  
   async downloadImg(options = {}) {
     let { filename } = await download.image(options);
-    return filename
-  }
-  
-  async cloudVision() {
-    await browser.newWindow(
-      "https://cloud.google.com/vision/docs/drag-and-drop"
-    );
-    const filePath = "./test/img/img.png";
-    const remoteFilePath = await browser.uploadFile(filePath);
-    await (await this.inputImg).setValue(remoteFilePath);
-    await (await this.robot).click();
+    return filename;
   }
 
-  
+  // async readImg(filename) {
+  //   const config = {
+  //     lang: "eng",
+  //     // oem: 1,
+  //     // psm: 3,
+  //   };
+  //   console.log("filename ",filename)
+  //   tesseract.recognize(filename, config)
+  //     .then((text) => {
+  //       console.log("Result:", text);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error.message);
+  //     });
+  // }
+
+  // async batchAnnotateFiles(fileName) {
+  //   const [result] = await client.textDetection(fileName);
+  //   const detections = result.textAnnotations;
+  //   console.log("Text:");
+  //   detections.forEach((text) => console.log(text));
+  // }
+
   /**
    * overwrite specifc options to adapt it to page object
    */
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,6 +2,7 @@ const LoginPage = require('../pageobjects/login.page');
 const ApiPage = require('../pageobjects/api.page')
 const CloudPage = require('../pageobjects/cloud.page')
 
+
 describe("My Login application",  () => {
   it("should login with valid credentials", async () => {
     // await LoginPage.open();
@@ -32,11 +33,11 @@ describe("My Login application",  () => {
 describe("Check Api", () => {
   it("should display the correct of course api", async () => {
     await CloudPage.open();
-    await CloudPage.reset();
+    await CloudPage.reset(process.env.TEST_EMAIL);
     let url = await CloudPage.imageCapcha();
     console.log("uri", url);
     // Set the download behavior of the page right before the download
-    // await browser.newWindow(url);
+
     // await browser.switchWindow(url);
     // await browser.cdp("Page", "setDownloadBehavior", {
     //   behavior: "allow",
@@ -50,8 +51,30 @@ describe("Check Api", () => {
     await CloudPage.downloadImg(options);
     // Give enough time to let the file download
     await browser.pause(5000);
+    const filePath = "./test/img/img.png";
 
-    await CloudPage.cloudVision();
+    await browser.newWindow('https://brandfolder.com/workbench/extract-text-from-image');
+    const el = await $(".fsp-drop-pane__input");
+    await el.scrollIntoView();
+    const remoteFilePath = await browser.uploadFile(filePath);
+
+    await el.setValue(remoteFilePath);
+    await browser.pause(10000);
+
+    const text = await $("textarea#extracted_text");
+    const getTextCapcha = await text.getText();
+    console.log("Text capcha:", getTextCapcha);
+    
+    await browser.pause(3000);
+    browser.switchWindow('https://id.ou.edu.vn/auth/forgotpwd');
+    const confirmCapcha = await $("input#form-captcha");
+    await confirmCapcha.setValue(getTextCapcha);
+
+    const btnConfirmCapcha = await $("button#resetbtn");
+    await btnConfirmCapcha.click();
+
+    // await CloudPage.batchAnnotateFiles(fileName);
+    // await CloudPage.readImg(fileName);
   });
 })
 
